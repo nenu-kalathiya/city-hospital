16fbe74505aebae1c14fe2ac66ba742c8beeaa0f
nenu-kalathiya <nenukalathiya111@gmail.com>
2022-06-13 20:56:52 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/header/Header';
+import Home from './container/home/Home';
+import Department from './container/Department/Department';
+import Doctor from './container/Doctor/Doctor';
+import About from './container/About/About';
+import Contact from './container/Contact/Contact';
+import Footer from './component/footer/Footer';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route path={"/"} exact component={Home}></Route>
+        <Route path="/Department" exact component={Department}></Route>
+        <Route path="/Doctor" exact component={Doctor}></Route>
+        <Route path="/About" exact component={About}></Route>
+        <Route path="/Contact" exact component={Contact}></Route>
+      </Switch>
+      <Home />
+      <Footer />
     </div>
   );
 }
